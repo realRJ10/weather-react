d067f810806248e9f98533b737807b94fb05eb6c
realRJ10 <121657363+realRJ10@users.noreply.github.com>
2023-07-25 15:47:53 +0330

    changed source code url

--- src/Coder.js
+++ src/Coder.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Coder() {
   return (
     <div className="coder">
-      <a href="https://github.com/realRJ10/my-weather-app" target="_blank">
+      <a href="https://github.com/realRJ10/weather-react" target="_blank">
         Open-source code{" "}
       </a>
       by Mojdeh Khorsand Moghadam
